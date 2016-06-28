bb84c64f21b7c83eabeaed36cf9353743ccbe315
daredia <shehzad.daredia@gmail.com>
2016-06-28 14:24:17 -0700

    'updated expressapp'

--- server/expressApp.js
+++ server/expressApp.js
@@ -8,8 +8,11 @@ myApp.get('/', function (req, res) {
 });
 
 myApp.get('/classes/messages', function (req, res) {
+  
   res.send('we are on classes messages');
 });
+
+
 
 myApp.listen(3000, '127.0.0.1', function () {
   console.log('Example app listening on port 3000!');
